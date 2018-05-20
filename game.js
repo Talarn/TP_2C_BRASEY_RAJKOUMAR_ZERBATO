9221645ef3cacbc3a889a4948e4c86c33fef6305
Talarn <39437252+Talarn@users.noreply.github.com>
2018-05-21 00:06:55 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,27 +1,24 @@
-const EventEmitter = require('events');
 const {Polis} = require('./polis');
 
 class Game {
   constructor() {
     this.polisList_ = [];
     this.polisNumber_ = 5;
-    for (let i = 0; i < this.polisNumber_; i++) {
-      this.polisList_.push(new Polis(this.polisNumber_, i));
-      this.polisList_[i].init();
+
+    for (let i = 0; i < this.polisNumber; i++) {
+      this.polisList.push(new Polis(this.polisNumber, i));
+      this.polisList[i].init();
     }
 
-    this.polisStateTimeFactor_ = 1000;
-    this.timeFactor_ = 1000;
-
-    this.worldEvent_ = new EventEmitter();
+    this.timeFactor_ = 10000;
   }
 
   init() {
-    this.polisList_.forEach(p => p.worldEvent_.on('trade', trade => {
+    this.polisList.forEach(p => p.worldEvent.on('trade', trade => {
       this.tradeCaravane(trade.a);
     }));
 
-    this.polisList_.forEach(p => p.worldEvent_.on('attack', attack => {
+    this.polisList.forEach(p => p.worldEvent.on('attack', attack => {
       this.fightBetweenPolis(attack.a, attack.b);
     }));
   }
@@ -29,35 +26,47 @@ class Game {
   fightBetweenPolis(a, b) {
     console.log('Armée de la ville '.red + a +
       ' marche vers ville '.red + b + ' !'.red);
-    this.polisList_[a].reserveArmySplitForAttack();
-    this.polisList_[b].reserveArmySplitForDefence();
+    this.polisList[a].reserveArmySplitForAttack();
+    this.polisList[b].reserveArmySplitForDefence();
 
     setTimeout(() => {
-      this.polisList_[a].attackingArmy_.forEach(u => u.fight());
-      this.polisList_[b].defendingArmy_.forEach(u => u.fight());
-    }, 4 * this.timeFactor * Math.random());
+      this.polisList[a].attackingArmy.forEach(u => u.fight());
+      this.polisList[b].defendingArmy.forEach(u => u.fight());
+    }, this.timeFactor);
 
     setTimeout(() => {
       console.log('== RESULTATS COMBAT ENTRE VILLE '.red +
         a + ' et '.red + b + ' =='.red);
-      this.polisList_[a].attackArmyBackToReserve();
-      this.polisList_[b].defenceArmyBackToReserve();
+      this.polisList[a].attackArmyBackToReserve();
+      this.polisList[b].defenceArmyBackToReserve();
       console.log(`...........Ville ${a}...........`);
-      this.polisList_[a].searchForSurvivor();
-      this.polisList_[a].divinityTouch();
+      this.polisList[a].searchForSurvivor();
+      this.polisList[a].divinityTouch();
       console.log(`...........Ville ${b}...........`);
-      this.polisList_[b].searchForSurvivor();
-      this.polisList_[b].divinityTouch();
-      console.log('========================================='.red);
-    }, 4 * this.timeFactor * Math.random());
+      this.polisList[b].searchForSurvivor();
+      this.polisList[b].divinityTouch();
+      console.log('======================'.red + '==================='.red);
+    }, this.timeFactor);
   }
 
   tradeCaravane(a) {
     console.log('Caravane au départ de '.rainbow + a + ' !'.rainbow);
 
     setTimeout(() => {
-      this.polisList_[a].trade();
-    }, 4 * this.timeFactor * Math.random());
+      this.polisList[a].trade();
+    }, 2 * this.timeFactor * (Math.random() + 0.1));
+  }
+
+  get timeFactor() {
+    return this.timeFactor_;
+  }
+
+  get polisList() {
+    return this.polisList_;
+  }
+
+  get polisNumber() {
+    return this.polisNumber_;
   }
 }
 module.exports = {Game};
